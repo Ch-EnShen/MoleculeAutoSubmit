8c87380c8fc9267e693a9aef6fb3a2677beced13
BrianShen <49614771+Ch-EnShen@users.noreply.github.com>
2024-07-04 12:42:16 +0800

    build: auto_create_and_submit.sh
    - cleaned_molecule_name
    remove the special chars in molecule name, e.g.,
    ",", "[", "]".
    for example,
    "2,3-dimethylbutane" --> "23-dimethylbutane"
    "Benzo[e]pyrene" --> "Benzoepyrene"
    
    - termination check function
    The pseudo-code is
    
    if state == s0
        if s0-opt-freq.log is error terminated
            print("error")
    	continue
        else
            print("normal")
    else if state == s1 and job_type == nacme
        if s1-nacme.log is error terminated
            print("error)
        else
            print("end job correctly!")
    	break
    else if state == s1
        if s1-opt-freq.log is error terminated
            print("error")
    	continue
        else
            print("normal")
    else
        unknown case
    
    CEShen

--- auto_create_and_submit.sh
+++ auto_create_and_submit.sh
@@ -20,7 +20,7 @@ nacme_path=$HOME/MOMAP_bak/momap/nacme
 if [ ! -z "$2" ]; then
   queue="$2"
 else
-  read -p 'Queue? ' queue
+  read -p 'Queue? (default to mem192v2)' queue
 fi
 
 if [ -z $queue ]; then
@@ -28,179 +28,235 @@ if [ -z $queue ]; then
 fi
 
 ncore=${queue_ncore[$queue]}
-
-# Reading of the nodes (default to 1)
-if [ ! -z "$3" ]; then
-  nnode="$3"
-else
-  read -p 'No. of Nodes? ' nnode
-fi
-
-if [ -z $nnode ]; then
-  nnode="1" #default
-fi
-
-# Read the jobname (default to "mol_sub_default")
-if [ ! -z "$4" ]; then
-  jobname="$4"
-else
-  read -p 'Jobname? ' jobname
-fi
-
-if [ -z $jobname ]; then
-  #jobname=$(cat qm.tpl | grep "sys_name" -A1 | tail -n 1 | cut -d'.' -f1)
-  jobname="mol_sub_default"
-fi
-
 ##############################################################
 
 ####################### define shell functions #######################
+# start job
+function start_job(){
+    # $1: cleaned molecule name
+    # $2: jobname
+cat << EOF > mol_calc_$1.sh
+#!/bin/bash
+#PBS -N $2
+#PBS -o $2.out
+#PBS -e $2.err
+#PBS -q $queue
+#PBS -l nodes=1:ppn=$ncore
+
+cd \$PBS_O_WORKDIR/
+module purge
+module load g16-a03
+module load anaconda3.8
+
+start=\$(date "+%s.%N")
+
+pip install pubchempy
+
+EOF
+}
+
 # modification of s0.gjf
-function g16_sed_s0_opt(){ # $1: molecule_name, $2: functional
+function g16_sed_s0_opt(){ 
+    # $1: molecule_name
     x_temp=$queue
     x=${x_temp#mem}
     mem_new=${x%v*}
-    sed -i "1c %chk=$1-$2-s0-opt-freq.chk" $1/$1-$2-s0-opt-freq.gjf
-    sed -i "2c %mem=${mem_new}GB" $1/$1-$2-s0-opt-freq.gjf
-    sed -i "3c %nprocshared=$ncore" $1/$1-$2-s0-opt-freq.gjf
+    sed -i "1c %chk=${1}-\${functional}-s0-opt-freq.chk" ${1}_\${functional}/${1}-\${functional}-s0-opt-freq.gjf
+    sed -i "2c %mem=${mem_new}GB" ${1}_\${functional}/$1-\${functional}-s0-opt-freq.gjf
+    sed -i "3c %nprocshared=$ncore" ${1}_\${functional}/$1-\${functional}-s0-opt-freq.gjf
+}
+
+function python_create_s0_gjf(){
+    # $1: molecule name
+    # $2: cleaned molecule name
+cat << EOF >> mol_calc_$2.sh
+    python generate_gjf_from_name.py $1 \$functional
+
+    for job in \$(jobs -p)
+    do
+        wait \$job
+    done
+
+EOF
 }
 
 # g16
+function g16job(){
+    # $1: clean_molecule_name
+    # $2: state (s0, s1)
+    # $3: job_type (opt-freq, nacme)
+cat << EOF >> mol_calc_$1.sh
+    INPUTFILE=${1}_\${functional}/$1-\${functional}-$2-$3.gjf
 
-function gen_g16wrapper_script(){
-cat << EOF >> g16_wrapper.sh
-#!/bin/bash
+    # Setup G09 runtime environment
+    OUTPUTFILE=\${INPUTFILE%.*}.log
+    export GAUSS_SCRDIR=/tmp/\$USER/\$RANDOM
+    mkdir -p \$GAUSS_SCRDIR
 
-# Check if the correct number of arguments are provided
-if [ \$# -lt 3 ]; then
-    echo "Usage: \$0 inputfile queue jobname"
-    exit 1
-fi
+    echo "======================================================="
+    echo "Working directory is" \$PBS_O_WORKDIR
+    echo "Starting on \$(hostname) at \$(date)"
 
-# Read arguments
-g16inputfile="\$1"
-queue="\$2"  # Default to 'workq' if not provided
-jobname="\$3"  # Default to input file name without extension
 
-# Create an expect script to handle the interactive prompts
-expect <<EOF
-set timeout -1
+    # Run g16 job
+    cd \$PBS_O_WORKDIR/
+    time g16 < \$INPUTFILE > \$OUTPUTFILE
 
-# Start the g16sub script
-spawn ~/bin/g16sub
+    # Clean scratch file once finished
+    rm -rf \$GAUSS_SCRDIR
 
-# Handle the input file prompt
-expect "G16 Input filename? " {
-    send "\$g16inputfile\r"
-}
-
-# Handle the queue prompt
-expect "Queue? " {
-    send "\$queue\r"
-}
-
-# Handle the jobname prompt
-expect "Jobname? " {
-    send "\$jobname\r"
-}
-
-# Wait for the process to complete
-expect eof
-\EOF
-EOF
-
-chmod u+x g16_wrapper.sh
-}
-
-function error_termination(){ 
-    # $1: mol_dir
-    # $2: file_name.gjf
-    
-    # Normal termination or Error termination
-    gjffile=$2
-    filename="${gjffile%.gjf}" # convert .gjf to .log
-
-    check=$(tail -n 10 ${1}/${filename}.log | grep 'Normal termination')
-    if [ -z "$check" ]; then 
-      echo "$2 process is Error terminated."
-      return 1
-    else
-      echo "$2 process is Normal terminated."
-    fi
-
-    formchk ${filename}.chk
-    mv $filename* $mol_dir/
-    return 0
-}
-
-function wait_jobs(){
-    for job in $(jobs -p)
+    for job in \$(jobs -p)
     do
-        wait $job
+        wait \$job
     done
-    sleep 1
+EOF
+}
+
+function python_mol_sub(){
+    # $1: clean_molecule_name
+    # $2: job_type (opt-freq, nacme)
+cat << EOF >> mol_calc_$1.sh
+    python runscript_mol_sub.py $1 $2 \$functional
+
+    for job in \$(jobs -p)
+    do
+        wait \$job
+    done
+EOF
 }
 
 # dushin
 function dushin_job(){ # for s0-s1 state
-	  # $1: molecule_name, $2: functional
-    mol_dir=${1}_${2}
-    cp $mol_dir/$1-$2-*-opt-freq.fchk $dushin_path
-    cp $mol_dir/$1-$2-*-opt-freq.log $dushin_path
+  # $1: cleaned molecule name
+cat << EOF >> mol_calc_$1.sh
+    cp \$PBS_O_WORKDIR/${1}_\${functional}/$1-\${functional}-*-opt-freq.fchk $dushin_path
+    cp \$PBS_O_WORKDIR/${1}_\${functional}/$1-\${functional}-*-opt-freq.log $dushin_path
 
     cd $dushin_path
-    sed -i '1s/g16/g09/g' $dushin_path/$1-$2-s0-opt-freq.log
-    sed -i '1s/g16/g09/g' $dushin_path/$1-$2-s1-opt-freq.log
+    sed -i '1s/g16/g09/g' $dushin_path/$1-\${functional}-s0-opt-freq.log
+    sed -i '1s/g16/g09/g' $dushin_path/$1-\${functional}-s1-opt-freq.log
 
-    change8="2 1 'ground state' '$1-$2-s1-opt-freq.log'"
-    change9="1 1 'excited state' '$1-$2-s0-opt-freq.log'"
-    sed -i "8c $change8" $dushin_path/dushin.inp
-    sed -i "9c $change9" $dushin_path/dushin.inp
+    change8="2 1 'ground state' '$1-\${functional}-s1-opt-freq.log'"
+    change9="1 1 'excited state' '$1-\${functional}-s0-opt-freq.log'"
+    sed -i "8c \$change8" $dushin_path/dushin.inp
+    sed -i "9c \$change9" $dushin_path/dushin.inp
 
-    source $dushin_path/run 2> $1/dushin_res.txt
+    source $dushin_path/run 2> \$PBS_O_WORKDIR/${1}_\${functional}/dushin_res.txt
 
-    cd $mol_sub_dir
-    cp $dushin_path/dushin_cart.dus $mol_dir
+    cp $dushin_path/dushin_cart.dus \$PBS_O_WORKDIR/${1}_\${functional}/
 
     rm $dushin_path/$1*
-    rm $dushin_path/dushin_cart.dus
+    rm dushin_cart.dus
+EOF
 }
 
 # nacme
-function nacme_job(){ 
-    # only for s1 state
-    # $1: mol_dir
-    # $2: s1 file name
-
-    cp $1/$2-opt-freq.log $nacme_path
-    cp $1/$2-nacme.log $nacme_path
+function nacme_job(){ # only for s1 state
+    # $1: cleaned molecule name
+cat << EOF >> mol_calc_$1.sh
+    cp \$PBS_O_WORKDIR/${1}_\${functional}/$1-\${functional}-s1-opt-freq.log $nacme_path
+    cp \$PBS_O_WORKDIR/${1}_\${functional}/$1-\${functional}-s1-nacme.log $nacme_path
 
     cd $nacme_path
-    sed -i "5s/get-nacme.*/get-nacme $2-nacme.log $2-opt-freq.log/g" $nacme_path/run
-    source $nacme_path/run 2> $1/nacme_res.txt
-    
-    cd $mole_sub_dir/$2
-    cp $nacme_path/nacme.out $1
-    cp $nacme_path/nacme.log $1
+    sed -i "5s/get-nacme.*/get-nacme $1-\${functional}-s1-nacme.log $1-\${functional}-s1-opt-freq.log/g" $nacme_path/run
 
-    rm $nacme_path/$2*
-    rm $nacme_path/nacme.out
-    rm $nacme_path/nacme.log
+    source $nacme_path/run 2> \$PBS_O_WORKDIR/${1}_\${functional}/nacme_res.txt
+    cp $nacme_path/nacme.out \$PBS_O_WORKDIR/${1}_\${functional}/
+    cp $nacme_path/nacme.log \$PBS_O_WORKDIR/${1}_\${functional}/
+
+    rm $nacme_path/$1*
+    rm nacme.out
+    rm nacme.log
+    
+    for job in \$(jobs -p)
+    do
+        wait \$job
+    done
+    
+    cd \$PBS_O_WORKDIR
+EOF
 }
 
 function get_trans_dip_adiabatic_energy(){
-    # $1: molecule_name, $2: functional
-    python runscript_get_dipole_and_energy.py $1 $2
+    # $1: cleaned molecule name
+cat << EOF >> mol_calc_$1.sh
+    python runscript_get_dipole_and_energy.py $1 \${functional}
+    for job in \$(jobs -p)
+    do
+        wait \$job
+    done
+
+    sleep 1
+EOF
 }
 
 function cp_to_ICrate_workdir(){
-    # $1: mol_dir
-    account=${HOME#/home/}
-    cp -r -T $1 /beegfs/hotpool/$account/Internal_Conversion/IC_Molecule/$1
+    # $1: cleaned molecule name
+cat << EOF >> mol_calc_$1.sh
+    account=\${HOME#/home/}
+    cd \$PBS_O_WORKDIR
+    cp -r -T ${1}_\${functional} /beegfs/hotpool/\$account/Internal_Conversion/IC_Molecule/${1}_\${functional}
+EOF
 }
 ###############################################################
 
-####################### exection part #######################
+#### check part
+function normal_termination(){
+    # Normal termination or Error termination
+
+    # $1: clean_molecule_name
+    # $2: state (s0, s1)
+    # $3: job_type (opt-freq, nacme)
+cat << EOF >> mol_calc_$1.sh
+    # mol_dir="${1}_\${functional}"
+    # filename=${1}-\${functional}-${2}-${3}
+    
+    formchk ${1}-\${functional}-${2}-${3}.chk
+    mv ${1}-\${functional}-${2}-${3}* ${1}_\${functional}/
+    
+    if [ "$2" == "s0" ]; then
+      check=\$(tail -n 1 "${1}_\${functional}/${1}-\${functional}-${2}-${3}.log" | grep 'Normal termination')
+      if [ -z "\$check" ]; then 
+        echo "${1}-\${functional}-${2}-${3} process is Error terminated."
+        continue
+      else
+        echo "${1}-\${functional}-${2}-${3} process is Normal terminated."
+      fi
+    elif [ "$2" == "s1" ] && [ "$3" == "nacme" ]; then
+      check=\$(tail -n 1 "${1}_\${functional}/${1}-\${functional}-${2}-${3}.log" | grep 'Normal termination')
+      if [ -z "\$check" ]; then
+        echo "${1}-\${functional}-${2}-${3} process is Error terminated."
+      else
+        echo "${1}-\${functional}-${2}-${3} process is Normal terminated."
+        break
+      fi
+    elif [ "$2" == "s1" ]; then
+      check=\$(tail -n 1 "${1}_\${functional}/${1}-\${functional}-${2}-${3}.log" | grep 'Normal termination')
+      if [ -z "\$check" ]; then
+        echo "${1}-\${functional}-${2}-${3} process is Error terminated."
+        continue
+      else
+        echo "${1}-\${functional}-${2}-${3} process is Normal terminated."
+      fi
+    else
+      echo "Unknown state: $3"
+      continue
+    fi
+EOF
+}
+
+function end_script(){
+cat << EOF >> rate_caln.sh
+end=\$(date "+%s.%N")
+
+echo "Job Ended at \$(date)"
+echo '======================================================='
+#mpdallexit
+EOF
+}
+####
+
+####################### execution part #######################
 functionals=(
     "b3lyp"
     "bmk"
@@ -208,59 +264,50 @@ functionals=(
     "camb3lyp"
 )
 
-# wrapper for g16sub
-gen_g16wrapper_script
-
 for molecule_name in $(cat < molecule_name.txt)
-do  
-    for functional in "${functionals[@]}"; do
-        # generate s1-opt-freq.gjf
-        python generate_gjf_from_name.py $molecule_name $functional & 
-
-        # remove undesired chars in molecule_name
-        cleaned_molecule_name=$(echo "$molecule_name" | tr -d ',[]')
-        mol_dir="${cleaned_molecule_name}_${functional}"
-
-        # s0 opt freq
-        g16_sed_s0_opt $cleaned_molecule_name $functional 
-        s0_INPUTFILE="${mol_dir}/$cleaned_molecule_name-$functional-s0-opt-freq.gjf"
-        ./g16_wrapper.sh $s0_INPUTFILE $queue $s0_INPUTFILE # g16sub s1-opt-freq.gjf
-        wait_jobs
-        if error_termination $mol_dir $s0_INPUTFILE; then # if Error termination, break the loop
-            rm -r $mol_dir
-            continue
-        fi
-
-        # s1 opt freq
-        python runscript_mol_sub.py $cleaned_molecule_name opt-freq $functional & # generate s1-opt-freq.gjf
-        s1_INPUTFILE="${mol_dir}/$cleaned_molecule_name-$functional-s1-opt-freq.gjf"
-        ./g16_wrapper.sh $s1_INPUTFILE $queue $s1_INPUTFILE # g16sub s1-opt-freq.gjf
-        wait_jobs
-        if error_termination $mol_dir $s1_INPUTFILE; then # if Error termination, break the loop
-            rm -r $mol_dir
-            continue
-        fi
-
-        get_trans_dip_adiabatic_energy $cleaned_molecule_name $functional & # save as transition dipole, adiabatic energy as .npy file
-        
-        # nacme
-        python runscript_mol_sub.py $cleaned_molecule_name nacme $functional & # generate s1-nacme.gjf
-        nacme_INPUTFILE="${mol_dir}/$cleaned_molecule_name-$functional-s1-nacme.gjf"
-        ./g16_wrapper.sh $nacme_INPUTFILE $queue $nacme_INPUTFILE # g16sub s1-nacme.gjf
-        wait_jobs
-        if error_termination $mol_dir $nacme_INPUTFILE; then # if Error termination, break the loop
-            rm -r $mol_dir
-            continue
-        fi
-
-        # dushin
-        dushin_job $cleaned_molecule_name $functional & # generate dushin_cart.dus
-        wait_jobs
-
-        # final check: break the functional loop if Normal termination
-        if !error_termination $mol_dir $s1_INPUTFILE; then # if Error termination, break the loop
-            break
-        fi
+do
+    cleaned_molecule_name=$(echo "$molecule_name" | tr -d ',[]')
+    
+    # setup for building molecular directory
+    for functional in "${functionals[@]}"
+    do
+      python generate_gjf_from_name.py $molecule_name $functional
     done
+    
+    # setup for qsub
+    start_job $cleaned_molecule_name $cleaned_molecule_name
+cat << EOF >> mol_calc_$cleaned_molecule_name.sh
+functionals=(
+    "b3lyp"
+    "bmk"
+    "pbe1pbe"
+    "camb3lyp"
+)
+
+for functional in "\${functionals[@]}"
+do
+EOF
+    # python_create_s0_gjf $molecule_name $cleaned_molecule_name # generate s0-opt-freq.gjf
+    g16job $cleaned_molecule_name s0 opt-freq # calculate s0-opt-freq
+    normal_termination $cleaned_molecule_name s0 opt-freq # check whether process error for s0-opt-freq
+    python_mol_sub $cleaned_molecule_name opt-freq # generate s1-opt-freq.gjf
+    g16job $cleaned_molecule_name s1 opt-freq # calculate s1-opt-freq
+    normal_termination $cleaned_molecule_name s1 opt-freq # check whether process error for s1-opt
+    get_trans_dip_adiabatic_energy $cleaned_molecule_name # save as transition dipole, adiabatic energy as .npy file
+    python_mol_sub $cleaned_molecule_name nacme # generate s1-nacme.gjf
+    g16job $cleaned_molecule_name s1 nacme # calculate s1-nacme
+    dushin_job $cleaned_molecule_name # generate dushin_cart.dus
+    nacme_job $cleaned_molecule_name # generate nacme.out
+    normal_termination $cleaned_molecule_name s1 nacme # check whether process error for s1-nacme
+
+cat << EOF >> mol_calc_$cleaned_molecule_name.sh
+done
+EOF
+
+end_script
+
+qsub mol_calc_$cleaned_molecule_name.sh
+rm mol_calc_$cleaned_molecule_name.sh
+
 done
 ###############################################################
